00606a83e25956a0dead0587677322e85d60cbda
feilong <feilongphone@gmail.com>
2018-10-16 14:29:32 +0800

    add home and fix cover

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -35,6 +35,12 @@ def Get(url):
 def index():
     addDirectoryItem(plugin.handle, plugin.url_for(
         show_search_input), ListItem("Search"), True)
+    p = Get('http://www.1993s.top')
+    it = re.finditer(r'<a class=\"stui-vodlist__thumb lazyload\" href=\"(.*?)\" title=\"(.*?)\" data-original=\"(.*?)\">',p)
+    for match in it:
+        id = re.search(r'(\d+)',match.group(1)).group()
+        addDirectoryItem(plugin.handle, plugin.url_for(show_detail, id=id, img=match.group(3)), ListItem(match.group(2),thumbnailImage=match.group(3)), True)
+
     endOfDirectory(plugin.handle)
 
 @plugin.route('/search_input')
@@ -50,41 +56,55 @@ def show_search_input():
         print match.group(2)
         # print id
         addDirectoryItem(plugin.handle, plugin.url_for(
-            show_search_resault, id), ListItem(match.group(2),thumbnailImage=match.group(3)), True)
+            show_detail, id=id, img=match.group(3)), ListItem(match.group(2),thumbnailImage=match.group(3)), True)
 
     endOfDirectory(plugin.handle)
 
-@plugin.route('/category/<category_id>')
-def show_category(category_id):
-    addDirectoryItem(
-        plugin.handle, "", ListItem("Hello category %s!" % category_id))
-    endOfDirectory(plugin.handle)
+# @plugin.route('/category/<category_id>')
+# def show_category(category_id):
+#     addDirectoryItem(
+#         plugin.handle, "", ListItem("Hello category %s!" % category_id))
+#     endOfDirectory(plugin.handle)
 
-@plugin.route('/Search/<Search_Text>')
-def show_search_resault(Search_Text):
-    p = Get('http://www.1993s.top/detail/' + Search_Text + '.html')
+@plugin.route('/Detail')
+def show_detail():
+    img = plugin.args['img'][0]
+    id = plugin.args['id'][0]
+    # print id
+    # print img
+    p = Get('http://www.1993s.top/detail/' + id + '.html')
     it = re.finditer(r'<a title=\"(.*?)\" href=\"(.*?)\">(?:.*?)</a>',p)
     for match in it:
-        id = re.search(r'(\d+-\d+-\d+)',match.group(2)).group()
-        addDirectoryItem(plugin.handle, plugin.url_for(play_Video, id), ListItem(match.group(1)), True)
+        vid = re.search(r'(\d+-\d+-\d+)',match.group(2)).group()
+        addDirectoryItem(plugin.handle, plugin.url_for(play_Video, video=vid, img=img), ListItem(match.group(1),thumbnailImage=img), True)
 
     endOfDirectory(plugin.handle)
 
-@plugin.route('/video/<video_url>')
-def play_Video(video_url):
-    print '=======================>'
-    print video_url
+@plugin.route('/video')
+def play_Video():
+    img = plugin.args['img'][0]
+    video_url = plugin.args['video'][0]
+    # print '=======================>'
+    # print video_url
     p = Get('http://www.1993s.top/video/' + video_url + '.html')
     match = re.search(r'\"url\":\"(.*?mp4)\"',p)
     print match.group(1).replace('\/','/')
     title = re.search(r'<h4 class=\"title\"><a href=\".*?\">(.*?)</a></h4>',p).group(1)
-    li = ListItem(title + ' ' + video_url)
-    # plot = re.search(r'<span class=\"left text-muted\">简介：</span><p><span style=\".*?\">(.*?)</span>',p).group(1)
+    li = ListItem(title + ' ' + video_url,thumbnailImage=img)
+
+    plot = ''
+    plotObj = re.search(r'<span class=\"left text-muted\">简介：</span><p>(.*?)</p>',p)
+    if plotObj:
+        plot = plotObj.group(1)
+
+    # print '=======================>'
+    # print plot
+
     li.setInfo("video",{
     #     'director': jsonArr['list']['anime']['author'],
-        'title': title
+        'title': title,
     #     'originaltitle': jsonArr['list']['anime']['name_jpn'],
-        # 'plot': plot
+        'plot': plot
     #     'sorttitle': jsonEpisodes['title'],
     #     'status': jsonArr['list']['anime']['is_ended']
     })
